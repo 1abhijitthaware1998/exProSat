2cf842c9086c846bce394ed91d7bbb940b8b309d
1abhijitthaware1998 <1abhijitthaware@gmail.com>
2020-09-06 21:19:35 +0530

    button added

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const app = express()
 app.listen(3000,()=>console.log('server running at 3000'))
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+
+app.post('/api',(req,res)=>{
+    console.log('I got a request')
+    console.log(req.body)
+    const data = req.body
+    res.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    })
+})
